450efd8fdd1aa34e5d1774d3303b9c2e0ca0b761
Zac M-W <zac@questionable.co.nz>
2017-04-13 21:04:45 +1200

    Added Weather file in preparation for implementation of weather factor

---
+++ scripts/weather.js
@@ -0,0 +1,24 @@
+// This is the file for calculating the score for weather
+// It will interact directly with the current score variable
+
+function timezone(placescores, weighting) {
+	// Schema = [
+	// 	{
+	// 		"ID": AirportID
+	// 		"Total": int_score,
+	// 		"Scores": {
+	// 			"flightlength": {
+	// 				score: totalscore,
+	// 				individuals: [array]
+	// 			},
+	// 			"timezone": {
+	// 				score: totalscore,
+	// 				individuals: [array]
+	// 			},
+	// 			"weather": {
+	// 				score: totalscore,
+	// 			}
+	// 		}
+	// 	}
+	// ]
+}
